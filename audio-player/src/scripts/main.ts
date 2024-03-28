1617cb93869feafec277135e75a895faf56122ed
nicolasdube123 <2242110@csfoy.ca>
2024-03-28 16:43:11 -0400

    prêt pour déploiement

--- audio-player/src/scripts/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import '../css/style.css'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
-import "bootstrap-icons/font/bootstrap-icons.css"
-
-import App from '../App.vue'
-
-createApp(App).mount('#app')
